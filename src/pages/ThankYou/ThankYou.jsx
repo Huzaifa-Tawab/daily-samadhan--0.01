2da223ad46eee81d9d00b1a85b7ed5d7086f02f7
Huzaifa Tawab <huzaifatawab33@gmail.com>
2024-01-03 17:00:52 +0500

    tnakyou

--- src/pages/ThankYou/ThankYou.jsx
+++ src/pages/ThankYou/ThankYou.jsx
@@ -2,17 +2,18 @@
 
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import './thanks.css'
+import "./thanks.css";
 
-const ThankYou = ({ props }) => {
-  const { Name } = useParams();
-  console.log(Name);
+const ThankYou = () => {
+  const { id } = useParams();
 
   return (
     <div className="thanks">
       <h2>Thank You for Submitting form we will reach you soon!</h2>
-      <p>Your ID is: {Name}</p>
-      <Link className="link-thanks" to="/">Go to Home</Link>
+      <p>Your ID is: {id}</p>
+      <Link className="link-thanks" to="/">
+        Go to Home
+      </Link>
     </div>
   );
 };
