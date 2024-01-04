69d7c56752146bf6fe3eb9c2768780b0b61151f0
Fasih <Fasih@softxion.com>
2024-01-04 19:17:55 +0500

    contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -275,16 +275,45 @@ function Contact(props) {
             <label className="errorLabel">{errors.phone}</label>
           </div>
           <div className="form-state">
-            <span>State</span>
-            <input
-              type="text"
-              value={State}
-              placeholder="e.g Mumbai"
-              onChange={(e) => setstate(e.target.value)}
-            />
-            <br />
-            <label className="errorLabel">{errors.state}</label>
-          </div>
+  <label htmlFor="state">State</label>
+  <select
+    id="state"
+    value={State}
+    onChange={(e) => setstate(e.target.value)}
+  >
+    <option value="">Select a State</option>
+    <option value="Andhra Pradesh">Andhra Pradesh</option>
+    <option value="Arunachal Pradesh">Arunachal Pradesh</option>
+    <option value="Assam">Assam</option>
+    <option value="Bihar">Bihar</option>
+    <option value="Chhattisgarh">Chhattisgarh</option>
+    <option value="Goa">Goa</option>
+    <option value="Gujarat">Gujarat</option>
+    <option value="Haryana">Haryana</option>
+    <option value="Himachal Pradesh">Himachal Pradesh</option>
+    <option value="Jharkhand">Jharkhand</option>
+    <option value="Karnataka">Karnataka</option>
+    <option value="Kerala">Kerala</option>
+    <option value="Madhya Pradesh">Madhya Pradesh</option>
+    <option value="Maharashtra">Maharashtra</option>
+    <option value="Manipur">Manipur</option>
+    <option value="Meghalaya">Meghalaya</option>
+    <option value="Mizoram">Mizoram</option>
+    <option value="Nagaland">Nagaland</option>
+    <option value="Odisha">Odisha</option>
+    <option value="Punjab">Punjab</option>
+    <option value="Rajasthan">Rajasthan</option>
+    <option value="Sikkim">Sikkim</option>
+    <option value="Tamil Nadu">Tamil Nadu</option>
+    <option value="Telangana">Telangana</option>
+    <option value="Tripura">Tripura</option>
+    <option value="Uttarakhand">Uttarakhand</option>
+    <option value="Uttar Pradesh">Uttar Pradesh</option>
+    <option value="West Bengal">West Bengal</option>
+  </select>
+  <br />
+  <label className="errorLabel">{errors.state}</label>
+</div>
           <span>Type of Dispute</span>
 
           <div className="form-dispute">
